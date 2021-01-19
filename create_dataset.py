12bd5a5f27f9913762404543f3881aac9e66ff59
Elmer <16076567@student.hhs.nl>
2021-01-19 23:47:03 +0100

    Fix dataset create script reversing the angle direction

--- create_dataset.py
+++ create_dataset.py
@@ -34,7 +34,7 @@ def get_list_of_labels(file):
         cx, cy, w, h, label, angle = rbox.split(' ')
         cx, cy, w, h, label, angle = float(cx), float(cy), float(w), float(h), float(label), float(angle)
 
-        angle = 2 * np.pi - angle * np.pi / 180
+        angle = angle * np.pi / 180
         img_file = file.split('/')[-1][:-5]
 
         l.append([img_file, cx, cy, w, h, angle, label])
